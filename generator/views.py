c61c6c12e347c9a45f7b6f7a457a6bc341f9a8d7
Saborna Roychowdhury <sabornaroy@gmail.com>
2022-03-07 17:28:56 -0600

    Our second commit

--- generator/views.py
+++ generator/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import random
 
 # Create your views here.
 
 def home(request):
     return render(request, 'generator/home.html')
+def about(request):
+    return render(request, 'generator/about.html')
 
 def password(request):
     return render(request, 'generator/password.html')
